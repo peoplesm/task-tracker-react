521f90ceb70b47b032d6310892ac7da7669a22d6
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-11-09 12:44:51 -0700

    initial Commit

---
+++ src/components/Task.js
@@ -0,0 +1,21 @@
+import { FaAtom } from 'react-icons/fa';
+
+const Task = ({ task, onDelete, onToggle }) => {
+  return (
+    <div
+      className={`task ${task.reminder ? 'reminder' : ''}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.text}
+        <FaAtom
+          style={{ color: 'red', cursor: 'pointer' }}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  );
+};
+
+export default Task;
